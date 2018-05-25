e87dd3bbad1b4522c838d9631d4ec49190b6f017
Michał Górny <mgorny@gentoo.org>
2018-05-25 15:56:59 +0200

    Replace fancy option parser with argparse

--- hyphenate.py
+++ hyphenate.py
@@ -1,42 +1,12 @@
 #!/usr/bin/env python2
 
+import argparse
 import codecs
 import locale
-import optparse
 import re
 import sys
 
 import pyphen
-
-
-class MultiArgOptionParser(optparse.OptionParser):
-    @staticmethod
-    def split_args(argv):
-        out = []
-        gotfile = False
-        for i, a in enumerate(argv):
-            if a.startswith('-'):
-                if a == '--':
-                    out.extend(argv[i:])
-                    break
-                elif gotfile:
-                    yield out
-                    out = []
-                    gotfile = False
-            else:
-                gotfile = True
-            out.append(a)
-        yield out
-
-    def parse_args(self, argv, values=None):
-        for arggroup in self.split_args(argv):
-            (values, args) = optparse.OptionParser.parse_args(
-                    self, arggroup, values)
-            yield (values, args)
-
-
-def reset_opts(opt, optstr, values, parser):
-    parser.values.__init__(defaults=parser.defaults)
 
 
 def main(argv):
@@ -44,61 +14,46 @@ def main(argv):
     deflang = locale.getlocale(locale.LC_COLLATE)[0] or 'en_GB'
     defenc = locale.nl_langinfo(locale.CODESET) or 'utf-8'
 
-    argparser = MultiArgOptionParser(
-            usage='%prog [opts1] file1 [...] [[opts2] file2 [...]] [...]')
+    argparser = argparse.ArgumentParser()
 
-    argparser.add_option(
+    argparser.add_argument(
         '-e', '--encoding', action='store',
         dest='encoding',
         help='Character encoding to use for I/O (locale default: %s)' % defenc)
-    argparser.add_option(
+    argparser.add_argument(
         '-l', '--language', action='store',
         dest='language',
         help='The language to lookup the hyphenation rules for '
              '(locale default: %s)' % deflang)
-    argparser.add_option(
-        '-r', '--reset', action='callback',
-        callback=reset_opts,
-        help='Reset the options to defaults (forfeit previous options)')
-    argparser.add_option(
+    argparser.add_argument(
         '-t', '--text', action='store_const',
         dest='type', const='text',
         help='Treat the following files as plain text files')
+    argparser.add_argument('file', nargs='+')
 
     argparser.set_defaults(
         encoding=defenc,
         language=deflang,
         type='text')
 
-    # Parse the arguments in series in order to apply the preceeding
-    # options to the filenames.
-    opts = None
-    for argn, (opts, args) in enumerate(argparser.parse_args(argv[1:])):
-        if not args:
-            if argn == 0:
-                argparser.error('no file specified')
-                return 1
-            else:
-                sys.stderr.write('Warning: options passed after '
-                                 'the last file have no effect.\n')
-        try:
-            h = pyphen.Pyphen(lang=opts.language)
-        except IOError as e:
-            argparser.error('unknown language: %s (%s)' % (
-                opts.language, str(e)))
-            # well, this probably won't be reached but keep it safe
-            continue
+    opts = argparser.parse_args()
 
-        for path in args:
-            try:
-                f = codecs.open(path, 'r+', opts.encoding)
-            except IOError as e:
-                sys.stderr.write('open() failed: %s\n' % str(e))
-            except LookupError as e:
-                argparser.error(str(e))
-            else:
-                if opts.type == 'text':
-                    hyph_text(f, h)
+    try:
+        h = pyphen.Pyphen(lang=opts.language)
+    except IOError as e:
+        argparser.error('unknown language: %s (%s)' % (
+            opts.language, str(e)))
+
+    for path in opts.file:
+        try:
+            f = codecs.open(path, 'r+', opts.encoding)
+        except IOError as e:
+            sys.stderr.write('open() failed: %s\n' % str(e))
+        except LookupError as e:
+            argparser.error(str(e))
+        else:
+            if opts.type == 'text':
+                hyph_text(f, h)
 
     return 0
 
